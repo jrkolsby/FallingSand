84d2e71591a2944e4ed0996e14e001d6118821c9
James Kolsby <jrkolsby@mac.com>
2019-04-23 14:40:08 -0400

    fix: copying

--- playground/sand.py
+++ playground/sand.py
@@ -2,22 +2,23 @@ HEIGHT = 10
 WIDTH = 10 
 
 def update(screen):
-    newscreen = screen.copy()
+    newscreen = [x.copy() for x in screen]
     for i in range(HEIGHT-1):
         for j in range(WIDTH-1):
             if screen[i][j] == 'X':
                 if (i+1 < HEIGHT and \
                     screen[i+1][j] == '.'):
-                    screen[i][j] = '.'
-                    screen[i+1][j] = 'X'
+                    newscreen[i][j] = '.'
+                    newscreen[i+1][j] = 'X'
                 elif (i+1 < HEIGHT and j > 0 and \
                       screen[i+1][j-1] == '.'):
-                    screen[i][j] = '.'
-                    screen[i+1][j-1] = 'X'
+                    newscreen[i][j] = '.'
+                    newscreen[i+1][j-1] = 'X'
                 elif (i+1 < HEIGHT and j+1 < WIDTH and \
                       screen[i+1][j+1] == '.'): # BOTTOM RIGHT
-                    screen[i][j] = '.'
-                    screen[i+1][j+1] = 'X'
+                    newscreen[i][j] = '.'
+                    newscreen[i+1][j+1] = 'X'
+    return newscreen
 
 def render(screen):
     output = ""
@@ -40,7 +41,8 @@ render(screen)
 
 while True:
     render(screen)
-    x = int(input("X: "))
-    y = int(input("Y: "))
-    add(screen, x, y)
-    update(screen)
+    x = input("X: ")
+    y = input("Y: ")
+    if x != '' and y != '':
+        add(screen, int(x), int(y))
+    screen = update(screen)
