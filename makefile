f386527f5a9765110cfc344e15834012da2c0e1a
jrkolsby <jrkolsby@mac.com>
2019-05-06 20:12:45 -0400

    feat: update module

--- makefile
+++ makefile
@@ -1,3 +1,7 @@
 .PHONY demo:
 demo: playground/sand.py
 	python3 playground/sand.py
+
+.PHONY hardware:
+hardware:
+	cd src/hw && make build
--- src/hw/modules/sand_top.sv
+++ src/hw/modules/sand_top.sv
@@ -1,33 +1,20 @@
-module sand_top(input [3:0] KEY,
-		
-		/*
-		// SLAVE FROM RAM
-		input [12:0] DRAM_ADDR,
-		input [1:0] DRAM_BA,
-		input DRAM_CAS_N,
-		input DRAM_CKE,
-		input DRAM_CLK,
-		input DRAM_CS_N,
-		inout [15:0] DRAM_DQ,
-		output DRAM_LDQM,
-		input DRAM_RAS_N,
-		input DRAM_UDQM,
-		input DRAM_WE_N,
-		*/
+module sand_top(
+	input logic reset,
+	input logic clock,
 
-		input logic reset,
-		input logic clock,
-
-		// SLAVE TO HPS
-		input logic write, 		// write enable
-		input logic [7:0] writedata,	// writedata
-		input chipselect,		// chipselect
-		input logic [2:0] address 	// address type
+	// SLAVE TO HPS
+	input logic write, 		// write enable
+	input logic [7:0] writedata,	// writedata
+	input chipselect,		// chipselect
+	input logic [2:0] address 	// address type
 );
 
     logic [7:0] background_r, background_g, background_b;
     logic [7:0] write_x, write_y, write_radius;
     logic [1:0] write_type;
+
+    logic [22:0] screen_buffer_a_ptr;
+    logic [22:0] screen_buffer_b_ptr;
 
     logic [17:0] kernel_buffer;
 
