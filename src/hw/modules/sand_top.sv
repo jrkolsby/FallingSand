6bfe2a375eeb39e1f3a6bf73843f60c9380ba3e7
jrkolsby <jrkolsby@mac.com>
2019-05-12 18:36:33 -0400

    feat: may 12

--- src/hw/modules/sand_top.sv
+++ src/hw/modules/sand_top.sv
@@ -11,6 +11,8 @@ module sand_top(
 	// GLOBALS
 	output logic [31:0] screen_a_ptr,
 	output logic [31:0] screen_b_ptr,
+	output logic [31:0] screen_c_ptr,
+
 	output logic [10:0] write_x,
 	output logic [9:0] write_y, 
 	output logic [1:0] write_t, 
@@ -20,8 +22,14 @@ module sand_top(
     logic [7:0] write_x, write_y, write_radius;
     logic [1:0] write_t;
 
+    logic screen_select;
     logic [31:0] screen_a_ptr;
     logic [31:0] screen_b_ptr;
+
+    logic [1:0] cacheselect;
+    wire [1279:0] screen_a_cache;
+    wire [1279:0] screen_b_cache;
+    wire [1279:0] screen_c_cache;
 
     always_ff @(posedge clock) begin
 	// RESET
@@ -39,6 +47,7 @@ module sand_top(
 		3'h2 : write_radius <= kernel_writedata[2:0];
 		3'h3 : write_t <= kernel_writedata[2:0];
 	    endcase
+	else if 
     end
 
 endmodule
