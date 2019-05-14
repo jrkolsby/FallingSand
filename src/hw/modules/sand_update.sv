283260f3c6de2f0e000938a38f33c8f4419e1ca1
Jeremy-Adkins <39724415+Jeremy-Adkins@users.noreply.github.com>
2019-05-14 00:33:14 -0400

    Added pixel registers

--- src/hw/modules/sand_update.sv
+++ src/hw/modules/sand_update.sv
@@ -1,53 +1,57 @@
 module sand_update(
-	input logic clock,
-	input logic reset,
-
-	input logic [31:0] screen_ptr,
-
-	output logic [23:0] mem_address,
-	output logic mem_read,
-	output logic mem_write,
-	input logic mem_waitrequest,
-	input logic mem_readdatavalid,
-	input logic [15:0] mem_readdata,
-	output logic [15:0] mem_writedata,
-
+	input logic docalculations,
 	input logic [15:0] region,
 	input logic [15:0] floor,
 	output logic [15:0] new_region,
-	output logic [15:0] new_floor
-    );
-
-    logic [16:0] write_block = 17'd0;
-
-    always_ff @(posedge clock) begin
-	if (mem_waitrequest == 1'b1) begin
-	    mem_write <= 1'd0; 
-	    write_block <= write_block + 17'd1;
-	end else begin
-	    mem_address <= write_block;
-	    mem_write <= 1'd1;
-	    mem_writedata <= 16'h2222;
+	output logic [15:0] new_floor);
+	
+	logic[1:0] r1;
+	assign r1 = region[15:14];
+	logic[1:0] r2;
+	assign r2 = region[13:12];
+	logic[1:0] r3;
+	assign r3 = region[15:10];
+	logic[1:0] r4;
+	assign r4 = region[15:8];
+	logic[1:0] r5;
+	assign r5 = region[7:6];
+	logic[1:0] r6;
+	assign r6 = region[5:4];
+	logic[1:0] r7;
+	assign r7 = region[3:2];
+	logic[1:0] r8;
+	assign r8 = region[1:0];
+	
+	logic[1:0] f1;
+	assign f1 = floor[15:14];
+	logic[1:0] f2;
+	assign f2 = floor[13:12];
+	logic[1:0] f3;
+	assign f3 = floor[15:10];
+	logic[1:0] f4;
+	assign f4 = floor[15:8];
+	logic[1:0] f5;
+	assign f5 = floor[7:6];
+	logic[1:0] f6;
+	assign f6 = floor[5:4];
+	logic[1:0] f7;
+	assign f7 = floor[3:2];
+	logic[1:0] f8;
+	assign f8 = floor[1:0];
+	
+	always @(docalculations) begin
+		for (i=7; i>=0; i=i-1) begin
+			else if (i == 7) begin//leftmost region pixel
+			
+			end
+			
+			if (i == 0) begin//rightmost region pixel
+				
+			end
+			
+			else begin//middle pixels
+			
+			end
+		end
 	end
-    end
-
 endmodule
-
-module check_region(
-	input logic [1:0] self_t,
-	input logic [1:0] topL_t,
-	input logic [1:0] topR_t,
-	input logic [1:0] top_t,
-	input logic [1:0] left_t,
-	input logic [1:0] right_t,
-	input logic [1:0] bottom_t,
-	input logic [1:0] bottomL_t,
-	input logic [1:0] bottomR_t,
-	output logic [1:0] dx,
-	output logic [1:0] dy);
-
-endmodule
-
-
-
-
